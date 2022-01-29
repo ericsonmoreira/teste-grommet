520c6670229a22f90c76c9b1ace16c745c16eecf
ericsonmoreira <moreira.ericson@gmail.com>
2022-01-29 16:52:22 -0300

    🔥 rm axios lib and auth context

--- src/hooks/useAuth.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext, AuthContextState } from '../context/AuthContext';
-
-export function useAuth(): AuthContextState {
-  const context = useContext(AuthContext);
-  return context;
-}
